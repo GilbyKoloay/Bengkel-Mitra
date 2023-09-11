66658298208244bd97715d48156be1d4aac4ac8c
GilbyKoloay <lolwtf10001@gmail.com>
2023-09-11 21:19:30 +0800

    add Nav to application and modified index.css

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import { Nav } from './components';
 import { Login, Home } from './pages';
 
 
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/login' element={<Login />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='*' element={<Navigate to='/home' />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='*' element={<Navigate to='/home' />} />
+      </Routes>
+    </>
   );
 };
