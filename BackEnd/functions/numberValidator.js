bab2d01bd4d28bcb16dd5932c19785b9347add7b
GilbyKoloay <lolwtf10001@gmail.com>
2023-09-11 18:13:53 +0800

    add documentValidator, stringValidator, and numberValidator functions

---
+++ BackEnd/functions/numberValidator.js
@@ -0,0 +1,22 @@
+export default function numberValidator(key, value, allowEmpty=false) {
+  if (!allowEmpty && !value) {
+    return (() => {
+      throw {
+        statusCode: 400,
+        description: `${key} tidak ada.`
+      }
+    })();
+  } else if (
+    typeof(value) === 'string' ||
+    Number.isNaN(value)
+  ) {
+    return (() => {
+      throw {
+        statusCode: 400,
+        description: `${key} tidak valid.`
+      }
+    })();
+  } else if (allowEmpty && !value) return null;
+  
+  return value;
+};
