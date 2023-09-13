6132dfcc05157554413528a4bea113abf05b360a
GilbyKoloay <lolwtf10001@gmail.com>
2023-09-13 20:43:48 +0800

    finished page/service pages

--- FrontEnd/src/pages/Service/Form/index.jsx
+++ FrontEnd/src/pages/Service/Form/index.jsx
@@ -1,45 +1,71 @@
 import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 
-import { Input, Button } from '../../../components';
-import { Fetch } from '../../../functions';
+import { Input, Button, Select } from '../../../components';
+import { Fetch, createSocket } from '../../../functions';
 
 
 
-export default function ServiceForm() {
+export default function TypeForm() {
   const navigate = useNavigate();
 
   const { formType } = useParams();
 
+  const [parameters] = useSearchParams();
+  const _id = parameters.get('_id');
+
+  const { types } = useSelector(state => state.app);
+
   const [name, setName] = useState('');
   const [type, setType] = useState('');
   const [subType, setSubType] = useState('');
-  const [priceClass1, setPriceClass1] = useState('');
-  const [priceClass2, setPriceClass2] = useState('');
-  const [priceClass3, setPriceClass3] = useState('');
-  const [priceClass4, setPriceClass4] = useState('');
-  const [priceClass5, setPriceClass5] = useState('');
+  const [price_class1, setPrice_class1] = useState('');
+  const [price_class2, setPrice_class2] = useState('');
+  const [price_class3, setPrice_class3] = useState('');
+  const [price_class4, setPrice_class4] = useState('');
+  const [price_class5, setPrice_class5] = useState('');
+  const [isFormLoadingInitialData, setIsFormLoadingInitialData] = useState((formType === 'create') ? false : true);
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
   const [formErrMsg, setFormErrMsg] = useState('');
 
 
 
   useEffect(() => {
-    validateFormType();
-  }, []);
-
-  useEffect(() => {
-    setFormErrMsg('');
-  }, [name, type, subType, priceClass1, priceClass2, priceClass3, priceClass4, priceClass5]);
-
-
-
-  function validateFormType() {
     if (
       formType !== 'create' &&
       formType !== 'update' &&
       formType !== 'delete'
-    ) navigate('/home');
+    ) navigate('/service');
+
+    if (
+      formType === 'update' ||
+      formType === 'delete'
+    ) loadFormInitialData();
+  }, []);
+
+  useEffect(() => {
+    setFormErrMsg('');
+  }, [name, type, subType, price_class1, price_class2, price_class3, price_class4, price_class5]);
+
+
+
+  async function loadFormInitialData() {
+    const res = await Fetch(`/service/get-all?_id=${_id}&name&type&subType&price`);
+    if (res?.ok) {
+      if (res.payload.length === 0) navigate('/service');
+      else {
+        setName(res.payload[0].name);
+        setType(res.payload[0].type._id);
+        setSubType(res.payload[0].subType);
+        setPrice_class1(res.payload[0].price.class1);
+        setPrice_class2(res.payload[0].price.class2);
+        setPrice_class3(res.payload[0].price.class3);
+        setPrice_class4(res.payload[0].price.class4);
+        setPrice_class5(res.payload[0].price.class5);
+        setIsFormLoadingInitialData(false);
+      }
+    }
   }
 
   async function formOnSubmit(e) {
@@ -47,36 +73,53 @@ export default function ServiceForm() {
     setIsFormSubmitting(true);
 
     const payload = {
-      name,
-      type,
-      subType,
+      _id,
+      name: name.trimEnd(),
+      type: type,
+      subType: subType?.trimEnd(),
       price: {
-        class1: priceClass1,
-        class2: priceClass2,
-        class3: priceClass3,
-        class4: priceClass4,
-        class5: priceClass5
+        class1: parseInt(price_class1),
+        class2: parseInt(price_class2),
+        class3: parseInt(price_class3),
+        class4: parseInt(price_class4),
+        class5: parseInt(price_class5)
       }
     };
-    // console.log('payload');
 
-    const res = await Fetch('/service/create', 'POST', payload);
+    const res = await Fetch(
+      `/service/${formType}`,
+      (formType === 'create') ? 'POST' : (formType === 'update') ? 'PUT' : 'DELETE',
+      payload
+    );
     if (res) {
       setIsFormSubmitting(false);
-      if (res.ok) navigate('/service');
+      if (res.ok) {
+        const socket = createSocket();
+        socket.emit(
+          `service-${(formType === 'create') ? 'new' : formType}`,
+          _id ? {_id} : undefined
+        );
+
+        navigate('/service');
+      }
       else setFormErrMsg(res.message);
     }
   }
 
-  function clearForm() {
+  function formClear() {
     setName('');
     setType('');
     setSubType('');
-    setPriceClass1('');
-    setPriceClass2('');
-    setPriceClass3('');
-    setPriceClass4('');
-    setPriceClass5('');
+    setPrice_class1('');
+    setPrice_class2('');
+    setPrice_class3('');
+    setPrice_class4('');
+    setPrice_class5('');
+  }
+
+  function formReset() {
+    setIsFormLoadingInitialData(true);
+    loadFormInitialData();
   }
 
 
@@ -84,100 +127,123 @@ export default function ServiceForm() {
   return (
     <main>
       <form className='h-full flex flex-col' onSubmit={formOnSubmit}>
-        <div className='text-xl'>{
-          (formType === 'create') ? 'Tambah '
-          : (formType === 'update') ? 'Perbarui '
-          : (formType === 'delete') ? 'Hapus ' : ''}
-          Layanan
-        </div>
+        <div className='text-xl'>{(formType === 'create') ? 'Tambah' : (formType === 'update') ? 'Ubah' : 'Hapus'} Layanan</div>
 
-        {isFormSubmitting && <div className='text-lg'>Sedang menambahkan data, mohon tunggu ...</div>}
+        {(isFormLoadingInitialData || !types) && <div className='text-lg'>Sedang memuat data, mohon tunggu ...</div>}
+        {(types?.length === 0) && <div className='text-lg text-red-500'>Tipe masih kosong, silahkan mengisi tipe terlebih dahulu.</div>}
+        {isFormSubmitting && <div className='text-lg'>Sedang {(formType === 'create') ? 'menambahkan' : (formType === 'update') ? 'mengubah' : 'menghapus'} data, mohon tunggu ...</div>}
         {formErrMsg && <div className='text-lg text-red-500'>{formErrMsg}</div>}
 
-        <div className='pt-4 pb-8 grid grid-cols-1 sm:grid-cols-2 sm:gap-8 md:gap-16 overflow-y-auto'>
-          <div>
-            <Input
-              label='Nama'
-              value={name}
-              onChange={value => setName(value)}
-              size='lg'
-            />
-            <Input
-              className='mt-4'
-              label='Tipe'
-              value={type}
-              onChange={value => setType(value)}
-              size='lg'
-            />
-            <Input
-              className='mt-4'
-              label='Subtipe'
-              value={subType}
-              onChange={value => setSubType(value)}
-              size='lg'
-            />
-          </div>
-          <div>
-            <Input
-              className='mt-4 sm:mt-0'
-              label='Harga Kelas 1'
-              value={priceClass1}
-              onChange={value => setPriceClass1(value)}
-              size='lg'
-            />
-            <Input
-              className='mt-4'
-              label='Harga Kelas 2'
-              value={priceClass2}
-              onChange={value => setPriceClass2(value)}
-              size='lg'
-            />
-            <Input
-              className='mt-4'
-              label='Harga Kelas 3'
-              value={priceClass3}
-              onChange={value => setPriceClass3(value)}
-              size='lg'
-            />
-            <Input
-              className='mt-4'
-              label='Harga Kelas 4'
-              value={priceClass4}
-              onChange={value => setPriceClass4(value)}
-              size='lg'
-            />
-            <Input
-              className='mt-4'
-              label='Harga Kelas 5'
-              value={priceClass5}
-              onChange={value => setPriceClass5(value)}
-              size='lg'
-            />
-          </div>
-        </div>
-        <div className='flex gap-2 sm:gap-4 md:gap-8'>
-          <Button
-            className='flex-1'
-            label='Kembali'
-            onClick={() => navigate('/service')}
-            size='lg'
-          />
-          <Button
-            className='flex-1'
-            label='Bersihkan'
-            onClick={clearForm}
-            size='lg'
-            color='red'
-          />
-          <Button
-            className='flex-1'
-            label='Tambah'
-            type='submit'
-            size='lg'
-            color='blue'
-            disabled={isFormSubmitting}
-          />
-        </div>
+        {(!isFormLoadingInitialData && types?.length > 0) && (
+          <>
+            <div className='pt-4 pb-8 grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-4 md:gap-8 overflow-y-auto'>
+              <div>
+                <Input
+                  label='Nama'
+                  value={name}
+                  onChange={value => setName(value.trimStart().toUpperCase())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+                <Select
+                  className='mt-4'
+                  label='Tipe'
+                  value={type}
+                  onChange={value => setType(value)}
+                  options={types.map(type => ([type._id, type.name]))}
+                  placeholder='(TIPE)'
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+                <Input
+                  className='mt-4'
+                  label='Subtipe'
+                  value={subType}
+                  onChange={value => setSubType(value.trimStart().toUpperCase())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+              </div>
+              <div>
+                <Input
+                  className='mt-4 sm:mt-0'
+                  label='Harga Kelas 1'
+                  value={price_class1}
+                  onChange={value => setPrice_class1(value.trim())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+                <Input
+                  className='mt-4'
+                  label='Harga Kelas 2'
+                  value={price_class2}
+                  onChange={value => setPrice_class2(value.trim())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+                <Input
+                  className='mt-4'
+                  label='Harga Kelas 3'
+                  value={price_class3}
+                  onChange={value => setPrice_class3(value.trim())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+                <Input
+                  className='mt-4'
+                  label='Harga Kelas 4'
+                  value={price_class4}
+                  onChange={value => setPrice_class4(value.trim())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+                <Input
+                  className='mt-4'
+                  label='Harga Kelas 5'
+                  value={price_class5}
+                  onChange={value => setPrice_class5(value.trim())}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+              </div>
+            </div>
+            <div className='flex gap-2 sm:gap-4 md:gap-8'>
+              <Button
+                className='flex-1'
+                label='Kembali'
+                onClick={() => navigate('/service')}
+                size='lg'
+              />
+              {(formType === 'create') ? (
+                <Button
+                  className='flex-1'
+                  label='Bersihkan'
+                  onClick={formClear}
+                  size='lg'
+                  color='red'
+                  disabled={isFormSubmitting}
+                />
+              ) : (formType === 'update') && (
+                <Button
+                  className='flex-1'
+                  label='Atur Ulang'
+                  onClick={formReset}
+                  size='lg'
+                  color='red'
+                  disabled={isFormLoadingInitialData || isFormSubmitting}
+                />
+              )}
+              <Button
+                className='flex-1'
+                label={(formType === 'create') ? 'Tambah' : (formType === 'update') ? 'Ubah' : 'Hapus'}
+                type='submit'
+                size='lg'
+                color={(formType === 'delete') ? 'red' : 'blue'}
+                disabled={isFormLoadingInitialData || isFormSubmitting}
+              />
+            </div>
+          </>
+        )}
       </form>
     </main>
   );
--- FrontEnd/src/pages/Service/index.jsx
+++ FrontEnd/src/pages/Service/index.jsx
@@ -1,7 +1,226 @@
-export default function Service() {
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, Input, Select } from '../../components';
+
+
+
+export default function Type() {
+  const navigate = useNavigate();
+
+  const { services, types } = useSelector(state => state.app);
+
+  const [filteredServices, setFilteredServices] = useState(null);
+  const [filter, setFilter] = useState({
+    name: '',
+    type: 'SEMUA',
+    subType: '',
+    priceClass1: '',
+    priceClass2: '',
+    priceClass3: '',
+    priceClass4: '',
+    priceClass5: ''
+  });
+
+
+
+  useEffect(() => {
+    if (services) {
+      const newFilteredServices = services.filter(service =>
+        (!filter.name || (service.name.includes(filter.name))) &&
+        ((filter.type === 'SEMUA') || (service.type.name.includes(filter.type))) &&
+        (!filter.subType || ((filter.subType === '-') && !service.subType) || (service.subType?.includes(filter.subType))) &&
+        (!filter.priceClass1 || (service.price.class1.toString().includes(filter.priceClass1))) &&
+        (!filter.priceClass2 || (service.price.class2.toString().includes(filter.priceClass2))) &&
+        (!filter.priceClass3 || (service.price.class3.toString().includes(filter.priceClass3))) &&
+        (!filter.priceClass4 || (service.price.class4.toString().includes(filter.priceClass4))) &&
+        (!filter.priceClass5 || (service.price.class5.toString().includes(filter.priceClass5)))
+      );
+      setFilteredServices(newFilteredServices);
+    }
+  }, [services, filter.name, filter.type, filter.subType, filter.priceClass1, filter.priceClass2, filter.priceClass3, filter.priceClass4, filter.priceClass5]);
+
+
+
+  function clearFilter() {
+    setFilter({
+      name: '',
+      type: 'SEMUA',
+      subType: '',
+      priceClass1: '',
+      priceClass2: '',
+      priceClass3: '',
+      priceClass4: '',
+      priceClass5: ''
+    });
+  }
+
+
+
   return (
     <main>
-      Service
+      <div>
+        <Button
+          label='Tambah Layanan'
+          onClick={() => navigate('/service/form/create')}
+          size='lg'
+          color='blue'
+        />
+      </div>
+
+      <div className='mt-4 overflow-auto'>
+        <table className='w-full border border-blue-500'>
+          <thead className='bg-blue-300'>
+            <tr className='text-lg border-y border-blue-500'>
+              {['Nama', 'Tipe', 'Subtipe'].map((title, index) => (
+                <th
+                  key={index}
+                  rowSpan={2}
+                  className='p-2 border-x border-blue-500'
+                >
+                  {title}
+                </th>
+              ))}
+              <th colSpan={5} className='p-2 border-x border-blue-500'>Harga</th>
+              <th rowSpan={2} className='p-2 border-x border-blue-500'>Aksi</th>
+            </tr>
+            <tr className='text-lg border-y border-blue-500'>
+              {['Kelas 1', 'Kelas 2', 'Kelas 3', 'Kelas 4', 'Kelas 5'].map((title, index) => <th key={index} className='p-2 border-x border-blue-500'>{title}</th>)}
+            </tr>
+            {((services?.length > 0) && (types?.length > 0)) && (
+              <>
+                <tr className='border-y border-blue-500'>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.name}
+                      onChange={value => setFilter({...filter, name: value.trimStart().toUpperCase()})}
+                      placeholder='filter nama'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Select
+                      className='font-normal'
+                      value={filter.type}
+                      options={[{name: 'SEMUA'}, ...types].map(type => [type.name, type.name])}
+                      onChange={value => setFilter({...filter, type: value})}
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.subType}
+                      onChange={value => setFilter({...filter, subType: value.trimStart().toUpperCase()})}
+                      placeholder='filter subtipe'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.priceClass1}
+                      onChange={value => setFilter({...filter, priceClass1: value.trim()})}
+                      placeholder='filter harga kelas 1'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.priceClass2}
+                      onChange={value => setFilter({...filter, priceClass2: value.trim()})}
+                      placeholder='filter harga kelas 2'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.priceClass3}
+                      onChange={value => setFilter({...filter, priceClass3: value.trim()})}
+                      placeholder='filter harga kelas 3'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.priceClass4}
+                      onChange={value => setFilter({...filter, priceClass4: value.trim()})}
+                      placeholder='filter harga kelas 4'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500'>
+                    <Input
+                      className='font-normal'
+                      value={filter.priceClass5}
+                      onChange={value => setFilter({...filter, priceClass5: value.trim()})}
+                      placeholder='filter harga kelas 5'
+                      size='md'
+                    />
+                  </th>
+                  <th className='p-2 border-x border-blue-500 w-0 whitespace-nowrap'>
+                    <Button
+                      className='w-full font-normal'
+                      label='Bersihkan Filter'
+                      onClick={clearFilter}
+                      size='md'
+                    />
+                  </th>
+                </tr>
+                <tr className='border-y border-blue-500'>
+                  <th colSpan={9} className='p-2 text-start'>Total: {filteredServices?.length} dari {services.length}</th>
+                </tr>
+              </>
+            )}
+          </thead>
+          <tbody>
+            {!services ? (
+              <tr>
+                <td colSpan={9} className='p-2 text-center'>Sedang memuat data mohon tunggu ...</td>
+              </tr>
+            ) : (services.length === 0) ? (
+              <tr>
+                <td colSpan={9} className='p-2 text-center'>Data kosong.</td>
+              </tr>
+            ) : (filteredServices?.length === 0) && (
+              <tr>
+                <td colSpan={9} className='p-2 text-center'>Data tidak ditemukan.</td>
+              </tr>
+            )}
+            {filteredServices?.map((service, index) => (
+              <tr key={index} className='odd:bg-neutral-200 even:bg-neutral-300 hover:bg-blue-300'>
+                <td className='p-2'>{service.name}</td>
+                <td className='p-2'>{service.type.name}</td>
+                <td className='p-2'>{service.subType ? service.subType : '-'}</td>
+                <td className='p-2'>{service.price.class1}</td>
+                <td className='p-2'>{service.price.class2}</td>
+                <td className='p-2'>{service.price.class3}</td>
+                <td className='p-2'>{service.price.class4}</td>
+                <td className='p-2'>{service.price.class5}</td>
+                <td className='p-2'>
+                  <div className='flex justify-center gap-4'>
+                    <Button
+                      label='Ubah'
+                      onClick={() => navigate(`/service/form/update?_id=${service._id}`)}
+                      color='yellow'
+                    />
+                    <Button
+                      label='Hapus'
+                      onClick={() => navigate(`/service/form/delete?_id=${service._id}`)}
+                      color='red'
+                    />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 };
