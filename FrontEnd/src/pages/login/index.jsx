3305063100c1a8966bb938c8e3e5b7fd94a5c39c
GilbyKoloay <lolwtf10001@gmail.com>
2023-09-14 01:21:25 +0800

    add color transition to background in login page

--- FrontEnd/src/pages/login/index.jsx
+++ FrontEnd/src/pages/login/index.jsx
@@ -10,12 +10,29 @@ import { set_token } from '../../redux/app';
 export default function Login() {
   const dispatch = useDispatch();
   
+  const [bgColorShade, setBgColorShade] = useState(100);
+  const [incrementBgColorShade, setIncrementBgColorShade] = useState(true);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
   const [formErrMsg, setFormErrMsg] = useState('');
 
 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (incrementBgColorShade) {
+        if (bgColorShade < 900) setBgColorShade(prev => prev + 100);
+        else setIncrementBgColorShade(false);
+      }
+      else {
+        if (bgColorShade > 100) setBgColorShade(prev => prev - 100);
+        else setIncrementBgColorShade(true);
+      }
+    }, 2500);
+
+    return () => clearInterval(interval);
+  }, [bgColorShade, incrementBgColorShade]);
 
   useEffect(() => {
     setFormErrMsg('');
@@ -28,7 +45,6 @@ export default function Login() {
     setIsFormSubmitting(true);
     
     const payload = {username, password};
-    // console.log('payload', payload);
 
     const res = await Fetch('/login', 'POST', payload);
     if (res) {
@@ -41,7 +57,7 @@ export default function Login() {
 
 
   return (
-    <main className='flex justify-center items-center gap-8'>
+    <main className={`flex justify-center items-center gap-8 bg-blue-${bgColorShade} transition-colors duration-[2500ms]`}>
       <form onSubmit={formOnSubmit}>
         <Input
           label='Nama Pengguna'
