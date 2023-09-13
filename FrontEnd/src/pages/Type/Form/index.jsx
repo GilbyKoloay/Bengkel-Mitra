585d52443b04790c4afa275a0ea2d1c687ab1400
GilbyKoloay <lolwtf10001@gmail.com>
2023-09-13 15:08:54 +0800

    refactor type/form page

--- FrontEnd/src/pages/Type/Form/index.jsx
+++ FrontEnd/src/pages/Type/Form/index.jsx
@@ -11,22 +11,27 @@ export default function TypeForm() {
 
   const { formType } = useParams();
 
-  const [parameters, setParameters] = useSearchParams();
+  const [parameters] = useSearchParams();
   const _id = parameters.get('_id');
 
   const [name, setName] = useState('');
-  const [isFormValid, setIsFormValid] = useState((formType === 'create') ? true : false);
+  const [isFormLoadingInitialData, setIsFormLoadingInitialData] = useState((formType === 'create') ? false : true);
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
   const [formErrMsg, setFormErrMsg] = useState('');
 
 
 
   useEffect(() => {
-    validateFormType();
+    if (
+      formType !== 'create' &&
+      formType !== 'update' &&
+      formType !== 'delete'
+    ) navigate('/home');
+
     if (
       formType === 'update' ||
       formType === 'delete'
-    ) getType();
+    ) loadFormInitialData();
   }, []);
 
   useEffect(() => {
@@ -35,19 +40,11 @@ export default function TypeForm() {
 
 
 
-  function validateFormType() {
-    if (
-      formType !== 'create' &&
-      formType !== 'update' &&
-      formType !== 'delete'
-    ) navigate('/home');
-  }
-
-  async function getType() {
+  async function loadFormInitialData() {
     const res = await Fetch(`/type/get-all?_id=${_id}&name`);
     if (res?.ok) {
       setName(res.payload[0].name);
-      setIsFormValid(true);
+      setIsFormLoadingInitialData(false);
     }
   }
 
@@ -56,11 +53,10 @@ export default function TypeForm() {
     setIsFormSubmitting(true);
 
     const payload = {_id, name};
-    // console.log('payload');
 
     const res = await Fetch(
       `/type/${formType}`,
-      (formType === 'create') ? 'POST' : 'PUT',
+      (formType === 'create') ? 'POST' : (formType === 'update') ? 'PUT' : 'DELETE',
       payload
     );
     if (res) {
@@ -75,8 +71,8 @@ export default function TypeForm() {
   }
 
   function formReset() {
-    setIsFormValid(false);
-    getType();
+    setIsFormLoadingInitialData(true);
+    loadFormInitialData();
   }
 
 
@@ -84,62 +80,62 @@ export default function TypeForm() {
   return (
     <main>
       <form className='h-full flex flex-col' onSubmit={formOnSubmit}>
-        <div className='text-xl'>{
-          (formType === 'create') ? 'Tambah '
-          : (formType === 'update') ? 'Ubah '
-          : (formType === 'delete') ? 'Hapus ' : ''}
-          Tipe
-        </div>
+        <div className='text-xl'>{(formType === 'create') ? 'Tambah' : (formType === 'update') ? 'Ubah' : 'Hapus'} Tipe</div>
 
-        {isFormSubmitting && <div className='text-lg'>Sedang menambahkan data, mohon tunggu ...</div>}
+        {isFormLoadingInitialData && <div className='text-lg'>Sedang memuat data, mohon tunggu ...</div>}
+        {isFormSubmitting && <div className='text-lg'>Sedang {(formType === 'create') ? 'menambahkan' : (formType === 'update') ? 'mengubah' : 'menghapus'} data, mohon tunggu ...</div>}
         {formErrMsg && <div className='text-lg text-red-500'>{formErrMsg}</div>}
 
-        <div className='pt-4 pb-8 grid grid-cols-1 overflow-y-auto'>
-          <div>
-            <Input
-              label='Nama'
-              value={name}
-              onChange={value => setName(value)}
-              size='lg'
-              disabled={!isFormValid || isFormSubmitting || (formType === 'delete')}
-            />
-          </div>
-        </div>
-        <div className='flex gap-2 sm:gap-4 md:gap-8'>
-          <Button
-            className='flex-1'
-            label='Kembali'
-            onClick={() => navigate('/type')}
-            size='lg'
-          />
-          {(formType === 'create') ? (
-            <Button
-              className='flex-1'
-              label='Bersihkan'
-              onClick={formClear}
-              size='lg'
-              color='red'
-              disabled={isFormSubmitting}
-            />
-          ) : (formType === 'update') && (
-            <Button
-              className='flex-1'
-              label='Atur Ulang'
-              onClick={formReset}
-              size='lg'
-              color='red'
-              disabled={!isFormValid || isFormSubmitting}
-            />
-          )}
-          <Button
-            className='flex-1'
-            label={(formType === 'create') ? 'Tambah' : (formType === 'update') ? 'Ubah' : 'Hapus'}
-            type='submit'
-            size='lg'
-            color={(formType === 'delete') ? 'red' : 'blue'}
-            disabled={!isFormValid || isFormSubmitting}
-          />
-        </div>
+        {!isFormLoadingInitialData && (
+          <>
+            <div className='pt-4 pb-8 grid grid-cols-1 overflow-y-auto'>
+              <div>
+                <Input
+                  label='Nama'
+                  value={name}
+                  onChange={value => setName(value)}
+                  size='lg'
+                  disabled={isFormLoadingInitialData || isFormSubmitting || (formType === 'delete')}
+                />
+              </div>
+            </div>
+            <div className='flex gap-2 sm:gap-4 md:gap-8'>
+              <Button
+                className='flex-1'
+                label='Kembali'
+                onClick={() => navigate('/type')}
+                size='lg'
+              />
+              {(formType === 'create') ? (
+                <Button
+                  className='flex-1'
+                  label='Bersihkan'
+                  onClick={formClear}
+                  size='lg'
+                  color='red'
+                  disabled={isFormSubmitting}
+                />
+              ) : (formType === 'update') && (
+                <Button
+                  className='flex-1'
+                  label='Atur Ulang'
+                  onClick={formReset}
+                  size='lg'
+                  color='red'
+                  disabled={isFormLoadingInitialData || isFormSubmitting}
+                />
+              )}
+              <Button
+                className='flex-1'
+                label={(formType === 'create') ? 'Tambah' : (formType === 'update') ? 'Ubah' : 'Hapus'}
+                type='submit'
+                size='lg'
+                color={(formType === 'delete') ? 'red' : 'blue'}
+                disabled={isFormLoadingInitialData || isFormSubmitting}
+              />
+            </div>
+          </>
+        )}
       </form>
     </main>
   );
