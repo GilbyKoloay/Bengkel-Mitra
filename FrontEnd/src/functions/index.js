d7a05db24088f6bca5710066396573c0f0b2f344
GilbyKoloay <lolwtf10001@gmail.com>
2023-09-18 11:36:23 +0800

    add getCurrentTime function

--- FrontEnd/src/functions/index.js
+++ FrontEnd/src/functions/index.js
@@ -3,7 +3,13 @@
 import Fetch from './Fetch';
 import createSocket from './createSocket';
 import splitString from './splitString';
+import getCurrentTime from './getCurrentTime';
 
 
 
-export { Fetch, createSocket, splitString };
+export {
+  Fetch,
+  createSocket,
+  splitString,
+  getCurrentTime
+};
