98a87dcbd957f794e53eb65f64a6b5a1758fb616
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-04 14:34:49 +0800

    applied main layout to transaction main page

--- FrontEnd/src/pages/Transaction/index.jsx
+++ FrontEnd/src/pages/Transaction/index.jsx
@@ -4,11 +4,13 @@ import { useNavigate } from 'react-router-dom';
 
 import {
   Button,
+  Table,
   InputDateTime,
   Input,
   Select
 } from '../../components';
 import { createTransactionInvoicePDF, splitString, toProperString } from '../../functions';
+import { Main } from '../../layouts';
 
 
 
@@ -77,195 +79,216 @@ export default function Transaction() {
 
 
   return (
-    <main>
-      <Button
-        className='whitespace-nowrap'
-        label='Tambah Transaksi'
-        onClick={() => navigate('/transaction/form/create')}
-        size='md'
-        theme='blue'
-      />
-
-      {!_transactions ? (
+    <Main
+      header={
+        <>
+          <Button
+            className='whitespace-nowrap'
+            label='Tambah Transaksi'
+            onClick={() => navigate('/transaction/form/create')}
+            size='md'
+            theme='blue'
+          />
+        </>
+      }
+      title='Data Transaksi'
+      table={!_transactions ? (
         <div className='mt-4 text-center text-xl'>Sedang memuat data, mohon tunggu ...</div>
       ) : (_transactions.length === 0) ? (
         <div className='mt-4 text-center text-xl'>Data kosong</div>
       ) : (
-        <div className='mt-8 overflow-auto'>
-          <div className='text-2xl'>Data Transaksi</div>
-          <table className='mt-4 w-full'>
-            <thead className='bg-blue-500 border-2 border-blue-700'>
-              <tr>
-                {['No. Faktur', 'Tanggal & Waktu', 'Nama Pelanggan'].map((title, index) => <th key={index} rowSpan={2} className='border border-blue-700 p-2'>{title}</th>)}
-                {['Kendaraan'].map((title, index) => <th key={index} colSpan={2} className='border border-blue-700 p-2'>{title}</th>)}
-                {['Layanan'].map((title, index) => <th key={index} colSpan={6} className='border border-blue-700 p-2'>{title}</th>)}
-                {['Total Harga', 'Status Bayar', 'Keterangan', 'Aksi'].map((title, index) => <th key={index} rowSpan={2} className='border border-blue-700 p-2'>{title}</th>)}
-              </tr>
-              <tr>
-                {['Jenis', 'Plat', 'Tipe', 'Subtipe', 'Nama', 'Kelas', 'Harga', 'Kuantitas'].map((title, index) => <th key={index} className='border border-blue-700 p-2'>{title}</th>)}
-              </tr>
-              <tr>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={invoiceNumber}
-                    onChange={value => setInvoiceNumber(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <InputDateTime
-                    value={dateTime}
-                    onChange={value => setDateTime(value)}
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={customerName}
-                    onChange={value => setCustomerName(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={vehicleType}
-                    onChange={value => setVehicleType(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={vehiclePlate}
-                    onChange={value => setVehiclePlate(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={service_type_name}
-                    onChange={value => setService_type_name(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={service_subType}
-                    onChange={value => setService_subType(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={service_name}
-                    onChange={value => setService_name(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={service_class}
-                    onChange={value => setService_class(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={service_price}
-                    onChange={value => setService_price(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={service_quantity}
-                    onChange={value => setService_quantity(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={totalPrice}
-                    onChange={value => setTotalPrice(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Select
-                    value={isPaid}
-                    options={[
-                      ['SEMUA', 'SEMUA'],
-                      [true, 'LUNAS'],
-                      [false, 'TIDAK LUNAS']
-                    ]}
-                    onChange={value => setIsPaid(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2'>
-                  <Input
-                    value={note}
-                    onChange={value => setNote(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th className='border border-blue-700 font-normal p-2 whitespace-nowrap'>
-                  <Button
-                    className='w-full'
-                    label='Bersihkan filter'
-                    onClick={clearFilter}
-                  />
-                </th>
-              </tr>
-              <tr>
-                <th colSpan={15} className='text-left p-2 font-normal'>Total {filtered_transactions().length} dari {_transactions.length}</th>
-              </tr>
-            </thead>
-            <tbody className='border-2 border-blue-700'>
-              {filtered_transactions().map(transaction => [0, 1].map(index => !index ? (
-                <tr key={index} className='border-t border-neutral-500'>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top text-end'>{transaction.invoiceNumber}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>{transaction.dateTime.slice(0, 10).split('-').reverse().join('-')} {transaction.dateTime.slice(11, 16)}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>{transaction.customerName}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>{transaction.vehicleType ? transaction.vehicleType : '-'}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>{transaction.vehiclePlate ? transaction.vehiclePlate : '-'}</td>
-                  <td className='p-2'>{transaction.services[0].type}</td>
-                  <td className='p-2'>{transaction.services[0].subType ? transaction.services[0].subType : '-'}</td>
-                  <td className='p-2'>{transaction.services[0].name}</td>
-                  <td className='p-2 text-center'>{transaction.services[0].class}</td>
-                  <td className='p-2 text-end'>{splitString(transaction.services[0].price, 3, '.')}</td>
-                  <td className='p-2 text-end'>{transaction.services[0].quantity}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top text-end'>{splitString(transaction.totalPrice, 3, '.')}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>{transaction.isPaid ? 'LUNAS' : 'TIDAK LUNAS'}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>{transaction.note ? transaction.note : '-'}</td>
-                  <td rowSpan={transaction.services.length} className='p-2 align-text-top'>
-                    <Select
-                      options={[
-                        'CETAK FAKTUR',
-                        'PERBARUI',
-                        'HAPUS'
-                      ].map(option => [option, option])}
-                      onChange={value => {
-                        if (value === 'CETAK FAKTUR') createTransactionInvoicePDF(transaction);
-                        else if (value === 'PERBARUI') navigate(`/transaction/form/update/${transaction._id}`);
-                        else if (value === 'HAPUS') navigate(`/transaction/form/delete/${transaction._id}`);
-                      }}
-                      placeholder='(Aksi)'
-                    />
-                  </td>
-                </tr>
-              ) : (transaction.services.length > 0) && transaction.services.map((service, thisIndex) => (thisIndex > 0) && (
-                <tr key={thisIndex} className='border-t border-neutral-500'>
-                  <td className='p-2'>{service.type}</td>
-                  <td className='p-2'>{service.subType ? service.subType : '-'}</td>
-                  <td className='p-2'>{service.name}</td>
-                  <td className='p-2 text-center'>{service.class}</td>
-                  <td className='p-2 text-end'>{splitString(service.price, 3, '.')}</td>
-                  <td className='p-2 text-end'>{service.quantity}</td>
-                </tr>
-              ))))}
-            </tbody>
-          </table>
-        </div>
+        <Table
+          titles={[
+            <>
+              {[
+                'No. Faktur',
+                'Tanggal & Waktu',
+                'Nama Pelanggan'
+              ].map((title, index) => <th key={index} rowSpan={2}>{title}</th>)}
+              {[
+                'Kendaraan'
+              ].map((title, index) => <th key={index} colSpan={2}>{title}</th>)}
+              {[
+                'Layanan'
+              ].map((title, index) => <th key={index} colSpan={6}>{title}</th>)}
+              {[
+                'Total Harga',
+                'Status Bayar',
+                'Keterangan',
+                'Aksi'
+              ].map((title, index) => <th key={index} rowSpan={2}>{title}</th>)}
+            </>,
+            <>
+              {[
+                'Jenis',
+                'Plat',
+                'Tipe',
+                'Subtipe',
+                'Nama',
+                'Kelas',
+                'Harga',
+                'Kuantitas'
+              ].map((title, index) => <th key={index}>{title}</th>)}
+            </>
+          ]}
+          filters={
+            <>
+              <th>
+                <Input
+                  value={invoiceNumber}
+                  onChange={value => setInvoiceNumber(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <InputDateTime
+                  value={dateTime}
+                  onChange={value => setDateTime(value)}
+                />
+              </th>
+              <th>
+                <Input
+                  value={customerName}
+                  onChange={value => setCustomerName(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={vehicleType}
+                  onChange={value => setVehicleType(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={vehiclePlate}
+                  onChange={value => setVehiclePlate(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={service_type_name}
+                  onChange={value => setService_type_name(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={service_subType}
+                  onChange={value => setService_subType(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={service_name}
+                  onChange={value => setService_name(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={service_class}
+                  onChange={value => setService_class(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={service_price}
+                  onChange={value => setService_price(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={service_quantity}
+                  onChange={value => setService_quantity(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={totalPrice}
+                  onChange={value => setTotalPrice(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Select
+                  value={isPaid}
+                  options={[
+                    ['SEMUA', 'SEMUA'],
+                    [true, 'LUNAS'],
+                    [false, 'TIDAK LUNAS']
+                  ]}
+                  onChange={value => setIsPaid(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Input
+                  value={note}
+                  onChange={value => setNote(value)}
+                  placeholder='filter'
+                />
+              </th>
+              <th>
+                <Button
+                  className='w-full'
+                  label='Bersihkan filter'
+                  onClick={clearFilter}
+                />
+              </th>
+            </>
+          }
+          info={<th colSpan={15}>Total {filtered_transactions().length} dari {_transactions.length}</th>}
+          data={filtered_transactions().map(transaction => [0, 1].map(index => !index ? (
+            <tr key={index}>
+              <td rowSpan={transaction.services.length} className='align-text-top text-right'>{transaction.invoiceNumber}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>{transaction.dateTime.slice(0, 10).split('-').reverse().join('-')} {transaction.dateTime.slice(11, 16)}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>{transaction.customerName}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>{transaction.vehicleType ? transaction.vehicleType : '-'}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>{transaction.vehiclePlate ? transaction.vehiclePlate : '-'}</td>
+              <td>{transaction.services[0].type}</td>
+              <td>{transaction.services[0].subType ? transaction.services[0].subType : '-'}</td>
+              <td>{transaction.services[0].name}</td>
+              <td className='text-center'>{transaction.services[0].class}</td>
+              <td className='text-right'>{splitString(transaction.services[0].price, 3, '.')}</td>
+              <td className='text-right'>{transaction.services[0].quantity}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top text-right'>{splitString(transaction.totalPrice, 3, '.')}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>{transaction.isPaid ? 'LUNAS' : 'TIDAK LUNAS'}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>{transaction.note ? transaction.note : '-'}</td>
+              <td rowSpan={transaction.services.length} className='align-text-top'>
+                <Select
+                  options={[
+                    'CETAK FAKTUR',
+                    'PERBARUI',
+                    'HAPUS'
+                  ].map(option => [option, option])}
+                  onChange={value => {
+                    if (value === 'CETAK FAKTUR') createTransactionInvoicePDF(transaction);
+                    else if (value === 'PERBARUI') navigate(`/transaction/form/update/${transaction._id}`);
+                    else if (value === 'HAPUS') navigate(`/transaction/form/delete/${transaction._id}`);
+                  }}
+                  placeholder='(Aksi)'
+                />
+              </td>
+            </tr>
+          ) : (transaction.services.length > 0) && transaction.services.map((service, thisIndex) => (thisIndex > 0) && (
+            <tr key={thisIndex} className='border-t border-neutral-500'>
+              <td>{service.type}</td>
+              <td>{service.subType ? service.subType : '-'}</td>
+              <td>{service.name}</td>
+              <td className='text-center'>{service.class}</td>
+              <td className='text-right'>{splitString(service.price, 3, '.')}</td>
+              <td className='text-right'>{service.quantity}</td>
+            </tr>
+          ))))}
+        />
       )}
-    </main>
+    />
   );
 };
