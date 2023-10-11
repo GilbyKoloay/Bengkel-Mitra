5b23d5cfff0d5badee5c64102c0ba4813ee9cfbf
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-11 22:29:01 +0800

    modified selected invoice page

--- FrontEnd/src/pages/Invoice/Selected/index.jsx
+++ FrontEnd/src/pages/Invoice/Selected/index.jsx
@@ -71,58 +71,58 @@ export default function InvoiceSelected() {
         {!invoice ? (
           <div className='text-xl'>Sedang memuat data, mohon tunggu ...</div>
         ) : (
-          <div className='flex-1 flex flex-col gap-4 overflow-auto'>
-            <div className='text-2xl'>Detail Faktur</div>
+          // <div className='flex-1 flex flex-col gap-4 overflow-auto'>
+          //   <div className='text-2xl'>Detail Faktur</div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 whitespace-nowrap'>
-              {[
-                ['Nama Pelanggan', invoice.customerName ? invoice.customerName : '-'],
-                ['Jenis Kendaraan', invoice.vehicleType ? invoice.vehicleType : '-'],
-                ['No. Polisi', invoice.vehiclePlate ? invoice.vehiclePlate : '-'],
-                ['Tanggal Masuk', invoice.entryDate ? toProperDateTime(invoice.entryDate, true) : '-'],
-                ['Tanggal Keluar', invoice.outDate ? toProperDateTime(invoice.outDate, true) : '-'],
-                ['Kilometer', invoice.kilometer ? invoice.kilometer : '-']
-              ].map(([key, value], index) => (
-                <div key={index} className='grid grid-cols-2'>
-                  <p>{key}</p>
-                  <p>: {value}</p>
-                </div>
-              ))}
-            </div>
+          //   <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 whitespace-nowrap'>
+          //     {[
+          //       ['Nama Pelanggan', invoice.customerName ? invoice.customerName : '-'],
+          //       ['Jenis Kendaraan', invoice.vehicleType ? invoice.vehicleType : '-'],
+          //       ['No. Polisi', invoice.vehiclePlate ? invoice.vehiclePlate : '-'],
+          //       ['Tanggal Masuk', invoice.entryDate ? toProperDateTime(invoice.entryDate, true) : '-'],
+          //       ['Tanggal Keluar', invoice.outDate ? toProperDateTime(invoice.outDate, true) : '-'],
+          //       ['Kilometer', invoice.kilometer ? invoice.kilometer : '-']
+          //     ].map(([key, value], index) => (
+          //       <div key={index} className='grid grid-cols-2'>
+          //         <p>{key}</p>
+          //         <p>: {value}</p>
+          //       </div>
+          //     ))}
+          //   </div>
 
-            <div className='flex-1'>
-              <table className='w-full border-2 border-neutral-900'>
-                <thead className='border-b border-neutral-900'>
-                  <tr>
-                    <th className='px-2 border-r border-neutral-900'>No.</th>
-                    <th className='px-2'>Uraian Pekerjaan</th>
-                    <th className='px-2 border-x border-neutral-900'>Harga</th>
-                    <th className='px-2'>Keterangan</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {invoice.services.map((service, index) => (
-                    <tr key={index} className={(index > 0) ? 'border-t border-neutral-900 border-dashed' : ''}>
-                      <td className='px-2 text-center border-r border-neutral-900'>{index+1}</td>
-                      <td className='px-2 whitespace-nowrap'>
-                        {service.primary.map((primary, index) => <div key={index}>{primary}</div>)}
-                        {service.secondary.map((secondary, index) => <div key={index}>• {secondary}</div>)}
-                      </td>
-                      <td className='px-2 text-end whitespace-nowrap border-x border-neutral-900'>Rp. {splitString(service.price, 3, '.')}</td>
-                      {(index === 0) && <td rowSpan={invoice.services.length} className='px-2'>{invoice.note ? invoice.note : ''}</td>}
-                    </tr>
-                  ))}
-                  <tr className='border-t border-neutral-900'>
-                    <td colSpan={2} className='px-2 font-bold text-center'>Jumlah</td>
-                    <td colSpan={2} className='px-2 font-bold text-center border-l border-neutral-900'>Rp. {splitString(invoice.totalPrice, 3, '.')}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          // <PDFViewer className='h-full'>
-          //   <PDF invoice={invoice} />
-          // </PDFViewer>
+          //   <div className='flex-1'>
+          //     <table className='w-full border-2 border-neutral-900'>
+          //       <thead className='border-b border-neutral-900'>
+          //         <tr>
+          //           <th className='px-2 border-r border-neutral-900'>No.</th>
+          //           <th className='px-2'>Uraian Pekerjaan</th>
+          //           <th className='px-2 border-x border-neutral-900'>Harga</th>
+          //           <th className='px-2'>Keterangan</th>
+          //         </tr>
+          //       </thead>
+          //       <tbody>
+          //         {invoice.services.map((service, index) => (
+          //           <tr key={index} className={(index > 0) ? 'border-t border-neutral-900 border-dashed' : ''}>
+          //             <td className='px-2 text-center border-r border-neutral-900'>{index+1}</td>
+          //             <td className='px-2 whitespace-nowrap'>
+          //               {service.primary.map((primary, index) => <div key={index}>{primary}</div>)}
+          //               {service.secondary.map((secondary, index) => <div key={index}>• {secondary}</div>)}
+          //             </td>
+          //             <td className='px-2 text-end whitespace-nowrap border-x border-neutral-900'>Rp. {splitString(service.price, 3, '.')}</td>
+          //             {(index === 0) && <td rowSpan={invoice.services.length} className='px-2'>{invoice.note ? invoice.note : ''}</td>}
+          //           </tr>
+          //         ))}
+          //         <tr className='border-t border-neutral-900'>
+          //           <td colSpan={2} className='px-2 font-bold text-center'>Jumlah</td>
+          //           <td colSpan={2} className='px-2 font-bold text-center border-l border-neutral-900'>Rp. {splitString(invoice.totalPrice, 3, '.')}</td>
+          //         </tr>
+          //       </tbody>
+          //     </table>
+          //   </div>
+          // </div>
+          <PDFViewer className='h-full'>
+            <PDF invoice={invoice} />
+          </PDFViewer>
         )}
       </div>
     </div>
