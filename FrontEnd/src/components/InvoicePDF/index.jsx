047d0f6b04c64eddeb8ddb65c9f9d1ccab1c7341
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-15 01:25:28 +0800

    refactor invoice pdf component

--- FrontEnd/src/components/InvoicePDF/index.jsx
+++ FrontEnd/src/components/InvoicePDF/index.jsx
@@ -6,143 +6,79 @@ import {
   Text
 } from '@react-pdf/renderer';
 
-import { splitString, toProperDateTime, getCurrentDateTime } from '../../functions';
+import { toProperDateTime, splitString } from '../../functions';
 
 
 
 const styles = StyleSheet.create({
   page: {
-    fontFamily: 'Times-Roman',
     padding: 32,
-  },
-
-  header: {
-    paddingBottom: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: 'black',
-
-    title: {
-      fontSize: 20
-    },
-    info: {
-      fontSize: 12
-    },
-    subInfo: {
-      fontSize: 8
-    }
+    fontFamily: 'Times-Roman',
+    fontSize: 12,
+    gap: 16
   },
 
   info: {
-    paddingTop: 16,
-    paddingBottom: 8,
+    width: '50%',
     flexDirection: 'row',
-
-    propertyWrapper: {
-      flexDirection: 'row'
-    },
-    text: {
-      fontSize: 12
-    },
-    colon: {
-      marginLeft: 16,
-      marginRight: 8
-    }
+    justifyContent: 'space-between'
   },
 
   table: {
-    marginTop: 8,
-    flexDirection: 'row',
-    borderWidth: 1.5,
-    borderColor: 'black',
+    border: '2px solid black',
 
-    notNoteWrapper: {
-      flex: 4
-    },
-    row: type => ({
+    row: row => ({
       flexDirection: 'row',
-      gap: 4,
-      borderBottomWidth: (type === 'data-last') ? 0 : 1,
-      borderBottomStyle: (type === 'head') ? 'solid' : 'dashed',
-      borderColor: 'black'
-    }),
-    colType: type => ({
-      flex:
-        (type === 'no') ? 1 :
-        (type === 'service') ? 7 :
-        (type === 'price') ? 2 : null,
-      justifyContent: 'center',
-      borderRightWidth: (type === 'no') ? 1 : 0,
-      borderLeftWidth: (type === 'price') ? 1 : 0,
-      borderColor: 'black'
-    }),
-    cell: (horAlign='left', fontSize='sm') => ({
-      fontSize:
-        (fontSize === 'sm') ? 12 :
-        (fontSize === 'lg') ? 14 : null,
-      textAlign: horAlign
-    }),
-    note: {
-      flex: 1,
-      borderLeftWidth: 1,
+      borderTopWidth: ((row !== 'title') && (row !== 'data-first')) ? 1 : 0,
+      borderBottomWidth: (row === 'title') ? 1 : 0,
       borderColor: 'black',
-
-      head: {
-        fontSize: 14,
-        textAlign: 'center',
-        borderBottomWidth: 1,
-        borderColor: 'black'
-      },
-      data: {
-        fontSize: 12,
-        paddingHorizontal: 4
-      }
+      borderStyle: (row === 'data') ? 'dashed' : 'solid'
+    }),
+    subRow: {
+      width: '91.666667%',
     },
-    total: {
-      paddingBottom: 8,
-      width: '50%',
-      alignSelf: 'flex-end',
-      flexDirection: 'row',
-      gap: 32,
-
-      text: {
-        fontSize: 14
-      }
-    }
-  },
-
-  footer: {
-    paddingTop: 8,
-    fontSize: 12,
-    textAlign: 'right'
+    col: col => ({
+      width:
+        (col === 'no') ? '8.333333%' :
+        (col === 'service') ? '50%' :
+        (col === 'price') ? '25%' :
+        (col === 'note') ? '25%' : 0,
+      padding: 2,
+      borderLeftWidth: ((col === 'service') || (col === 'price')) ? 1 : 0,
+      borderRightWidth: (col === 'price') ? 1 : 0,
+      borderColor: 'black'
+    })
   }
 });
 
 
 
-export default function PDF({ invoice }) {
-  console.log('invoice', invoice);
+export default function InvoicePDF({ invoice }) {
   return (
     <Document>
       <Page size='A4' style={styles.page}>
-        <View style={styles.header}>
-          <Text style={styles.header.title}>Mitra Oto</Text>
-          <Text style={styles.header.info}>Jl. Pingkan Matindas No. 48, Dendengan Dalam</Text>
-          <Text style={styles.header.subInfo}>No. HP: 081356071990</Text>
+        <View>
+          <Text style={{fontSize: 20}}>Mitra Oto</Text>
+          <Text>Jl. Pingkan Matindas No. 48, Dendengan Dalam</Text>
+          <Text style={{fontSize: 8}}>No. HP: 081356071990</Text>
         </View>
+
+
+
+        <View style={{border: '1px solid black', marginVertical: -8}} />
+
+
 
         <View style={styles.info}>
           <View>
             {[
-              'Nama Customer',
+              'Nama Pelanggan',
               'Jenis Kendaraan',
               'Nomor Polisi',
               'Tanggal Masuk',
               'Tanggal Keluar',
               'Kilometer'
-            ].map((key, index) => <Text key={index} style={styles.info.text}>{key}</Text>)}
-          </View>
-          <View>
-            {[...new Array(6)].map(index => <Text key={index} style={[styles.info.text, styles.info.colon]}>:</Text>)}
+            ].map((key, index) => <Text key={index} style={{}}>{key}</Text>)}
           </View>
           <View>
             {[
@@ -152,49 +88,68 @@ export default function PDF({ invoice }) {
               invoice.entryDate ? toProperDateTime(invoice.entryDate, true) : '',
               invoice.outDate ? toProperDateTime(invoice.outDate, true) : '',
               invoice.kilometer
-            ].map((value, index) => <Text key={index} style={styles.info.text}>{value ? value : '-'}</Text>)}
+            ].map((value, index) => <Text key={index} style={{}}>: {value ? value : '-'}</Text>)}
           </View>
         </View>
+
+
 
         <View style={styles.table}>
-          <View style={styles.table.notNoteWrapper}>
-            <View style={styles.table.row('head')}>
-              <View style={styles.table.colType('no')}>
-                <Text style={styles.table.cell('center', 'lg')}>No.</Text>
-              </View>
-              <View style={styles.table.colType('service')}>
-                <Text style={styles.table.cell('center', 'lg')}>Uraian Pekerjaan</Text>
-              </View>
-              <View style={styles.table.colType('price')}>
-                <Text style={styles.table.cell('center', 'lg')}>Harga</Text>
+          <View style={[styles.table.row('title'), {fontSize: 14}]}>
+            <Text style={[styles.table.col('no'), {textAlign: 'center'}]}>No</Text>
+            <View style={[styles.table.subRow, {flexDirection: 'row'}]}>
+              <Text style={[styles.table.col('service'), {textAlign: 'center'}]}>Uraian Pekerjaan</Text>
+              <Text style={[styles.table.col('price'), {textAlign: 'center'}]}>Harga</Text>
+              <Text style={[styles.table.col('note'), {textAlign: 'center'}]}>Keterangan</Text>
+            </View>
+          </View>
+          {invoice.services.map((subServices, index) => (
+            <View key={index} style={styles.table.row(index ? 'data' : 'data-first')}>
+              <Text style={[styles.table.col('no'), {textAlign: 'center'}]}>{index+1}</Text>
+              <View style={styles.table.subRow}>
+                {subServices.map((subService, subIndex) => (
+                  <View key={subIndex} style={{flexDirection: 'row'}}>
+                    <View style={styles.table.col('service')}>
+                      <Text>{subService.name}</Text>
+                      {subService.paid && <Text style={{textAlign: 'right'}}>Panjar</Text>}
+                    </View>
+                    <View style={[styles.table.col('price'), {justifyContent: 'space-between'}]}>
+                      {subService.price ? (
+                        <Text style={{textAlign: 'right'}}>Rp. {splitString(subService.price, 3, '.')}</Text>
+                      ) : (
+                        <Text style={{textAlign: 'center'}}>-</Text>
+                      )}
+                      {subService.paid && <Text style={{textAlign: 'right'}}>Rp. {splitString(subService.paid, 3, '.')}</Text>}
+                    </View>
+                    <Text style={styles.table.col('note')}>{subService.note}</Text>
+                  </View>
+                ))}
               </View>
             </View>
-            {invoice.services.map((service, index) => (
-              <View key={index} style={styles.table.row(`data${(index === invoice.services.length-1) ? '-last' : ''}`)}>
-                <View style={styles.table.colType('no')}>
-                  <Text style={styles.table.cell('center')}>{index+1}</Text>
-                </View>
-                <View style={styles.table.colType('service')}>
-                  {service.primary.map((primary, subIndex) => <Text key={subIndex} style={styles.table.cell()}>{primary}</Text>)}
-                  {service.secondary.map((secondary, subIndex) => <Text key={subIndex} style={styles.table.cell()}>- {secondary}</Text>)}
-                </View>
-                <View style={styles.table.colType('price')}>
-                  <Text style={styles.table.cell('right')}>Rp. {splitString(service.price, 3, '.')} </Text>
-                </View>
-              </View>
-            ))}
+          ))}
+          <View style={[styles.table.row('total'), {fontSize: 14}]}>
+            <View style={[styles.table.col('no')]} />
+            <View style={[styles.table.subRow, {flexDirection: 'row'}]}>
+              <Text style={[styles.table.col('service'), {textAlign: 'center'}]}>Jumlah</Text>
+              <Text style={[styles.table.col('price'), {textAlign: 'right'}]}>Rp. {splitString(invoice.totalPrice, 3, '.')}</Text>
+              <View style={[styles.table.col('note')]} />
+            </View>
           </View>
-          <View style={styles.table.note}>
-            <Text style={styles.table.note.head}>Keterangan</Text>
-            <Text style={styles.table.note.data}>{invoice.note}</Text>
-          </View>
-        </View>
-        <View style={styles.table.total}>
-          <Text style={styles.table.total.text}>Jumlah</Text>
-          <Text style={styles.table.total.text}>: Rp. {splitString(invoice.totalPrice, 3, '.')}</Text>
         </View>
 
-        <Text style={styles.footer}>Manado, {toProperDateTime(getCurrentDateTime(), true)}</Text>
+
+
+        <View style={{gap: 8}}>
+          <View>
+            <Text>*catatan:</Text>
+            {invoice.notes.map((note, index) => (
+              <Text key={index} style={{marginLeft: 4}}>- {note}</Text>
+            ))}
+          </View>
+          <View>
+            <Text style={{textAlign: 'right'}}>{invoice.city} - {toProperDateTime(invoice.createDate, true)}</Text>
+          </View>
+        </View>
       </Page>
     </Document>
   );
