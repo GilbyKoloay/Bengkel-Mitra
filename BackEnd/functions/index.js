59b295252d9d6a49f1d8ea3c19957b7f1a01541c
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-25 00:34:57 +0800

    removed unnecessary files and codes (removed type, service, and transaction

--- BackEnd/functions/index.js
+++ BackEnd/functions/index.js
@@ -2,20 +2,16 @@
 
 import Res from './Res.js';
 import Json from './Json.js';
-// import documentValidator from './documentValidator.js';
 import stringValidator from './stringValidator.js';
 import numberValidator from './numberValidator.js';
 import dateTimeValidator from './dateTimeValidator.js';
-import booleanValidator from './booleanValidator.js';
 
 
 
 export {
   Res,
   Json,
-  // documentValidator,
   stringValidator,
   numberValidator,
-  dateTimeValidator,
-  booleanValidator
+  dateTimeValidator
 };
--- BackEnd/router.js
+++ BackEnd/router.js
@@ -1,30 +1,10 @@
 import express from 'express';
 
-import {
-  // service as serviceController,
-  // Type as typeController,
-  // transaction as transactionController,
-  invoice as invoiceController
-} from './controllers/index.js';
+import { invoice as invoiceController } from './controllers/index.js';
 
 
 
 const router = express.Router();
-
-// router.post('/service', serviceController.post);
-// router.get('/service', serviceController.get);
-// router.put('/service', serviceController.put);
-// router.delete('/service', serviceController.delete);
-
-// router.post('/type', typeController.post);
-// router.get('/type', typeController.get);
-// router.put('/type', typeController.put);
-// router.delete('/type', typeController.delete);
-
-// router.post('/transaction', transactionController.post);
-// router.get('/transaction', transactionController.get);
-// router.put('/transaction', transactionController.put);
-// router.delete('/transaction', transactionController.delete);
 
 router.post('/invoice', invoiceController.post);
 router.get('/invoice', invoiceController.get);
