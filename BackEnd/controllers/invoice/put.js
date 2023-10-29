eecc7df7e9d1e32bfd584e8c896b928ea3fd4d15
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-29 16:28:41 +0800

    modified invoice/put controller

--- BackEnd/controllers/invoice/put.js
+++ BackEnd/controllers/invoice/put.js
@@ -8,42 +8,66 @@ import {
 
 
 
-export default async function post(req, res) {
+export default function put(req, res) {
   try {
     const payload = {
       _id: stringValidator(req.body?._id),
-      createDate: dateTimeValidator(req.body?.createDate),
+
+      headerLabels: {
+        top: stringValidator(req.body?.headerLabels?.top),
+        mid: stringValidator(req.body?.headerLabels?.mid),
+        bot: stringValidator(req.body?.headerLabels?.bot)
+      },
+
       customerName: stringValidator(req.body?.customerName),
       vehicleType: stringValidator(req.body?.vehicleType),
       vehiclePlate: stringValidator(req.body?.vehiclePlate),
       entryDate: dateTimeValidator(req.body?.entryDate),
       outDate: dateTimeValidator(req.body?.outDate),
-      kilometer: numberValidator(req.body?.kilometer),
-      services: req.body?.services?.map(subServices => (
-        subServices?.map(subService => ({
+      kilometer: stringValidator(req.body?.kilometer),
+      info: req.body?.info?.map(item => ({
+        label: stringValidator(item?.label),
+        value: (item?.type === 'date') ? dateTimeValidator(item?.value) : stringValidator(item?.value),
+        type: stringValidator(item?.type),
+      })),
+
+      priceShow: stringValidator(req.body?.priceShow),
+      paidShow: stringValidator(req.body?.paidShow),
+      tableLabels: {
+        col1: stringValidator(req.body?.tableLabels?.col1),
+        col2: stringValidator(req.body?.tableLabels?.col2),
+        col3: stringValidator(req.body?.tableLabels?.col3),
+        col4: stringValidator(req.body?.tableLabels?.col4),
+        paid: stringValidator(req.body?.tableLabels?.paid),
+        totalPaid: stringValidator(req.body?.tableLabels?.totalPaid),
+        totalPrice: stringValidator(req.body?.tableLabels?.totalPrice),
+        calculated: stringValidator(req.body?.tableLabels?.calculated)
+      },
+      services: req.body?.services?.map(service => ({
+        no: stringValidator(service?.no),
+        subServices: service?.subServices?.map(subService => ({
           name: stringValidator(subService?.name),
-          price: numberValidator(subService?.price),
-          paid: numberValidator(subService?.paid),
-          note: stringValidator(subService?.note)
+          price: stringValidator(subService?.price),
+          paid: stringValidator(subService?.paid),
+          note: stringValidator(subService?.note),
         }))
-      )),
-      totalPrice: numberValidator(req.body?.totalPrice) ? parseInt(req.body.totalPrice) : 0,
+      })),
+      totalPaid: stringValidator(req.body?.totalPaid),
+      totalPrice: stringValidator(req.body?.totalPrice),
+      calculated: numberValidator(req.body?.calculated),
+
+      noteLabel: stringValidator(req.body?.noteLabel),
       notes: req.body?.notes?.map(note => stringValidator(note)),
-      city: stringValidator(req.body?.city)
+      paymentLabels: {
+        top: stringValidator(req.body?.paymentLabels?.top),
+        mid: stringValidator(req.body?.paymentLabels?.mid),
+        bot: stringValidator(req.body?.paymentLabels?.bot)
+      },
+
+      city: stringValidator(req.body?.city),
+      createDate: dateTimeValidator(req.body?.createDate)
     };
-    if (!payload.createDate) return Res(res, 400, null, 'Tanggal pembuatan tidak valid');
-    if (payload.services.length === 0) return Res(res, 400, null, 'Pekerjaan tidak valid');
-    else {
-      let invalid = false;
-
-      payload.services.forEach(subServices => {
-        subServices.forEach(subService => {
-          if (!subService.name) invalid = true;
-        });
-      });
-
-      if (invalid) return Res(res, 400, null, 'Pekerjaan tidak valid');
-    }
+    // console.log('payload', payload);
 
     const data = Json('invoice');
     
