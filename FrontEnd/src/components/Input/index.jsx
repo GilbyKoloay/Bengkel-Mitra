46026e656972d8e0c00bc9691863323ba277caf2
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-29 16:26:54 +0800

    modified input component

--- FrontEnd/src/components/Input/index.jsx
+++ FrontEnd/src/components/Input/index.jsx
@@ -1,49 +1,26 @@
 export default function Input({
-  className='',
-  label='',
-  value='',
-  onChange=null,
+  className,
+  value,
+  onChange,
+  placeholder,
   type='text',
-  placeholder='',
-  disabled=false,
-  size='sm',
-  theme='neutral',
-  errMsg=''
+  disabled=false
 }) {
   return (
-    <div
-      className={`flex flex-col
+    <input
+      className={`
         ${className}
+        w-full p-1 border border-neutral-700 rounded placeholder:text-neutral-500 focus:outline focus:outline-1
+        ${!disabled ?
+          'bg-neutral-100 focus:outline-neutral-700' :
+          'bg-neutral-500 hover:cursor-not-allowed'
+        }
       `}
-    >
-      {label && (
-        <label
-          className={
-            (size === 'sm') ? 'text-base' :
-            (size === 'md') ? 'text-lg' :
-            (size === 'lg') ? 'text-xl' : ''
-          }
-        >
-          {label}
-        </label>
-      )}
-      <input
-        className={`w-full border-2 focus:outline focus:outline-1 focus:outline-offset-1 placeholder:text-neutral-500
-          ${(size === 'sm') ? 'py-1 px-1 text-sm' :
-            (size === 'md') ? 'py-1 px-2 text-base' :
-            (size === 'lg') ? 'py-2 px-2 text-lg' : ''
-          }
-          ${disabled ? 'bg-neutral-500 border-neutral-700 rounded hover:cursor-not-allowed' : (
-            (theme === 'neutral') ? 'bg-neutral-300 border-neutral-700 rounded focus:outline-neutral-700' : ''
-          )}
-        `}
-        value={value}
-        onChange={e => onChange(e.target.value)}
-        placeholder={placeholder}
-        type={type}
-        disabled={disabled}
-      />
-      {errMsg && <label className='text-red-500'>{errMsg}</label>}
-    </div>
+      value={value}
+      onChange={e => onChange(e.target.value)}
+      placeholder={placeholder}
+      type={type}
+      disabled={disabled}
+    />
   );
 };
