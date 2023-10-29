63a03ca4996e853b4d23a72c2079d4b6a9df2e76
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-29 16:25:52 +0800

    modified getCurrentDateTime function

--- FrontEnd/src/functions/getCurrentDateTime.js
+++ FrontEnd/src/functions/getCurrentDateTime.js
@@ -3,13 +3,5 @@ import { DateTime } from 'luxon';
 
 
 export default function getCurrentDateTime() {
-  const dateTime = DateTime.now().setZone('Asia/Singapore').toISO();
-
-  let year = dateTime.split('T')[0].split('-')[0];
-  let month = dateTime.split('T')[0].split('-')[1];
-  let day = dateTime.split('T')[0].split('-')[2];
-  let hour = dateTime.split('T')[1].split(':')[0];
-  let minute = dateTime.split('T')[1].split(':')[1];
-
-  return `${year}-${month}-${day}T${hour}:${minute}:00.000Z`;
+  return `${DateTime.now().setZone('Asia/Singapore').toISO().slice(0, 23)}Z`;
 };
