9fe63a374d2eadd44a6d22c229ad636988fc6df7
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-29 16:27:59 +0800

    modified stringValidator function

--- BackEnd/functions/stringValidator.js
+++ BackEnd/functions/stringValidator.js
@@ -2,7 +2,7 @@ export default function stringValidator(value, deleteAllSpace=false) {
   if (
     (!deleteAllSpace && !value?.replace(/ +/g, ' ').trim()) ||
     (deleteAllSpace && !value?.replaceAll(' ', ''))
-  ) return null;
+  ) return '';
   
   if (!deleteAllSpace) return value.replace(/ +/g, ' ').trim();
   return value.replaceAll(' ', '');
