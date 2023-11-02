7d5e7667a82abdc7679cd15e95d163a534d33fcb
GilbyKoloay <lolwtf10001@gmail.com>
2023-11-03 05:33:01 +0800

    removed filter inputs in invoice main page

--- FrontEnd/src/pages/Invoice/index.jsx
+++ FrontEnd/src/pages/Invoice/index.jsx
@@ -22,10 +22,9 @@ export default function Invoice() {
   const [customerName, setCustomerName] = useState('');
   const [vehicleType, setVehicleType] = useState('');
   const [vehiclePlate, setVehiclePlate] = useState('');
-  const [entryDate, setEntryDate] = useState('--T00:00:00.000Z');
-  const [outDate, setOutDate] = useState('--T00:00:00.000Z');
+  const [inDateOutDate, setInDateOutDate] = useState('--T00:00:00.000Z');
   const [kilometer, setKilometer] = useState('');
-  const [totalPrice, setTotalPrice] = useState('');
+  const [calculated, setCalculated] = useState('');
 
 
 
@@ -37,14 +36,11 @@ export default function Invoice() {
       (!customerName || ((customerName === '-') && !invoice.customerName) || invoice.customerName?.includes(toProperString(customerName))) &&
       (!vehicleType || ((vehicleType === '-') && !invoice.vehicleType) || invoice.vehicleType?.includes(toProperString(vehicleType))) &&
       (!vehiclePlate || ((vehiclePlate === '-') && !invoice.vehiclePlate) || invoice.vehiclePlate?.includes(toProperString(vehiclePlate))) &&
-      (!entryDate.split('T')[0].split('-')[2] || invoice.entryDate?.split('T')[0]?.split('-')[2]?.includes(entryDate.split('T')[0].split('-')[2])) &&
-      (!entryDate.split('T')[0].split('-')[1] || invoice.entryDate?.split('T')[0]?.split('-')[1]?.includes(entryDate.split('T')[0].split('-')[1])) &&
-      (!entryDate.split('T')[0].split('-')[0] || invoice.entryDate?.split('T')[0]?.split('-')[0]?.includes(entryDate.split('T')[0].split('-')[0])) &&
-      (!outDate.split('T')[0].split('-')[2] || invoice.outDate?.split('T')[0]?.split('-')[2]?.includes(outDate.split('T')[0].split('-')[2])) &&
-      (!outDate.split('T')[0].split('-')[1] || invoice.outDate?.split('T')[0]?.split('-')[1]?.includes(outDate.split('T')[0].split('-')[1])) &&
-      (!outDate.split('T')[0].split('-')[0] || invoice.outDate?.split('T')[0]?.split('-')[0]?.includes(outDate.split('T')[0].split('-')[0])) &&
+      (!inDateOutDate.split('T')[0].split('-')[2] || invoice.inDateOutDate?.split('T')[0]?.split('-')[2]?.includes(inDateOutDate.split('T')[0].split('-')[2])) &&
+      (!inDateOutDate.split('T')[0].split('-')[1] || invoice.inDateOutDate?.split('T')[0]?.split('-')[1]?.includes(inDateOutDate.split('T')[0].split('-')[1])) &&
+      (!inDateOutDate.split('T')[0].split('-')[0] || invoice.inDateOutDate?.split('T')[0]?.split('-')[0]?.includes(inDateOutDate.split('T')[0].split('-')[0])) &&
       (!kilometer || ((kilometer === '-') && !invoice.kilometer) || invoice.kilometer?.toString()?.includes(toProperString(kilometer))) &&
-      (!totalPrice || ((totalPrice === '-') && !invoice.totalPrice) || invoice.totalPrice?.toString()?.includes(toProperString(totalPrice)))
+      (!calculated || ((calculated === '-') && !invoice.calculated) || invoice.calculated?.toString()?.includes(toProperString(calculated)))
     );
   }
 
@@ -58,7 +54,7 @@ export default function Invoice() {
             <Button
               className='whitespace-nowrap'
               label='Tambah Faktur'
-              onClick={() => navigate('/invoice/form/create')}
+              onClick={() => navigate('/invoice/form')}
               size='md'
               theme='blue'
             />
@@ -78,95 +74,86 @@ export default function Invoice() {
                   'Nama Pelanggan',
                   'Jenis Kendaraan',
                   'No. Polisi',
-                  'Tanggal Masuk',
-                  'Tanggal Keluar',
+                  'Tgl Msk/Tgl Klr',
                   'Kilometer',
-                  'Total Harga'
+                  'Jumlah'
                 ].map((title, index) => <th key={index}>{title}</th>)}
               </>
             ]}
-            filters={
-              <>
-                <th>
-                  <InputDate
-                    useTime={false}
-                    value={createDate}
-                    onChange={value => setCreateDate(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <Input
-                    value={customerName}
-                    onChange={value => setCustomerName(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <Input
-                    value={vehicleType}
-                    onChange={value => setVehicleType(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <Input
-                    value={vehiclePlate}
-                    onChange={value => setVehiclePlate(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <InputDate
-                    useTime={false}
-                    value={entryDate}
-                    onChange={value => setEntryDate(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <InputDate
-                    useTime={false}
-                    value={outDate}
-                    onChange={value => setOutDate(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <Input
-                    value={kilometer}
-                    onChange={value => setKilometer(value)}
-                    placeholder='filter'
-                  />
-                </th>
-                <th>
-                  <Input
-                    value={totalPrice}
-                    onChange={value => setTotalPrice(value)}
-                    placeholder='filter'
-                  />
-                </th>
-              </>
-            }
-            info={<th colSpan={10}>Total {filtered_invoices().length} dari {_invoices.length}</th>}
+            // filters={
+            //   <>
+            //     <th>
+            //       <InputDate
+            //         useTime={false}
+            //         value={createDate}
+            //         onChange={value => setCreateDate(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //     <th>
+            //       <Input
+            //         value={customerName}
+            //         onChange={value => setCustomerName(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //     <th>
+            //       <Input
+            //         value={vehicleType}
+            //         onChange={value => setVehicleType(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //     <th>
+            //       <Input
+            //         value={vehiclePlate}
+            //         onChange={value => setVehiclePlate(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //     <th>
+            //       <InputDate
+            //         useTime={false}
+            //         value={inDateOutDate}
+            //         onChange={value => setInDateOutDate(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //     <th>
+            //       <Input
+            //         value={kilometer}
+            //         onChange={value => setKilometer(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //     <th>
+            //       <Input
+            //         value={calculated}
+            //         onChange={value => setCalculated(value)}
+            //         placeholder='filter'
+            //       />
+            //     </th>
+            //   </>
+            // }
+            // info={<th colSpan={9}>Total {filtered_invoices().length} dari {_invoices.length}</th>}
+            info={<th colSpan={9}>Total {filtered_invoices().length}</th>}
             data={filtered_invoices()?.map((invoice, index) => (
               <tr key={index} className='hover:cursor-pointer'>
                 {[
                   [invoice?.createDate?.slice(0, 10)?.split('-')?.reverse()?.join('-'), ''],
-                  [invoice?.customerName, ''],
-                  [invoice?.vehicleType, ''],
-                  [invoice?.vehiclePlate, 'whitespace-nowrap'],
-                  [invoice?.entryDate?.slice(0, 10)?.split('-')?.reverse()?.join('-'), ''],
-                  [invoice?.outDate?.slice(0, 10)?.split('-')?.reverse()?.join('-'), ''],
-                  [invoice?.kilometer, ''],
-                  [`Rp. ${splitString(invoice?.totalPrice, 3, '.')}`, 'text-right whitespace-nowrap']
+                  [invoice?.info?.filter(info => info?.label?.toLowerCase()?.trim() === 'nama pelanggan')[0]?.value, ''],
+                  [invoice?.info?.filter(info => info?.label?.toLowerCase()?.trim() === 'jenis kendaraan')[0]?.value, ''],
+                  [invoice?.info?.filter(info => info?.label?.toLowerCase()?.trim() === 'nomor polisi')[0]?.value, 'whitespace-nowrap'],
+                  [invoice?.info?.filter(info => info?.label?.toLowerCase()?.trim() === 'tgl msk/tgl klr')[0]?.value?.slice(0, 10)?.split('-')?.reverse()?.join('-'), 'whitespace-nowrap'],
+                  [invoice?.info?.filter(info => info?.label?.toLowerCase()?.trim() === 'kilometer')[0]?.value, 'whitespace-nowrap'],
+                  [`Rp. ${splitString(invoice?.calculated, 3, '.')}`, 'text-right whitespace-nowrap']
                 ].map(([value, className], index) => (
                   <td
                     key={index}
                     className={className}
-                    onClick={() => navigate(`/invoice/${invoice._id}`)}
+                    onClick={() => navigate(`/invoice/form?_id=${invoice._id}`)}
                   >
-                    {value ? value : '-'}
+                    {value}
                   </td>
                 ))}
               </tr>
